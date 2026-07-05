4079d323bf2a1240db1a42cd8239f1e91aec6a95
pchengi <prashanth.chengi@gmail.com>
2026-07-05 23:21:32 +0200

    Use local app theme helper

--- app/src/main/java/com/pchengi/tasklistspro/MainActivity.kt
+++ app/src/main/java/com/pchengi/tasklistspro/MainActivity.kt
@@ -18,7 +18,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
-import com.pchengi.tasklistspro.ui.theme.TasklistsProTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
