cf788d3d997b531c4dea111d194f5c010f43a4a3
pchengi <prashanth.chengi@gmail.com>
2026-07-05 23:39:13 +0200

    Add lifecycle compose dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,6 +42,7 @@ kotlin {
 dependencies {
     implementation("androidx.core:core-ktx:1.15.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.7")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.7")
     implementation("androidx.activity:activity-compose:1.9.3")
 
     implementation(platform("androidx.compose:compose-bom:2024.12.01"))
